14fd4c6085e2139240b741249b50929beab23032
shadowfeind <travestroy007@gmail.com>
2024-10-29 18:20:05 +0545

    update workspace hook

--- src/features/workspaces/route.ts
+++ src/features/workspaces/route.ts
@@ -1,6 +1,6 @@
 import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
-import { createWorkspaceSchema } from "./schemas";
+import { createWorkspaceSchema, updateWorkspaceSchema } from "./schemas";
 import { sessionMiddleware } from "@/lib/session-middleware";
 import {
   DATABASE_ID,
@@ -11,6 +11,7 @@ import {
 import { ID, Query } from "node-appwrite";
 import { MemberRoles } from "../members/types";
 import { generateInviteCode } from "@/lib/utils";
+import { getMember } from "../members/utils";
 
 // do not put comma at the end of the middleware
 const app = new Hono()
@@ -78,6 +79,60 @@ const app = new Hono()
         workspaceId: workspace.$id,
         role: MemberRoles.ADMIN,
       });
+
+      return c.json({ data: workspace });
+    }
+  )
+  .patch(
+    "/:workspaceId",
+    sessionMiddleware,
+    zValidator("form", updateWorkspaceSchema),
+    async (c) => {
+      const databases = c.get("databases");
+      const storage = c.get("storage");
+      const user = c.get("user");
+
+      const { name, image } = c.req.valid("form");
+      const { workspaceId } = c.req.param();
+
+      const member = await getMember({
+        databases,
+        workspaceId,
+        userId: user.$id,
+      });
+      if (!member || member.role !== MemberRoles.ADMIN) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
+
+      let uploadedImgUrl: string | undefined;
+      if (image instanceof File) {
+        const file = await storage.createFile(
+          IMAGES_BUCKER_ID,
+          ID.unique(),
+          image
+        );
+
+        const arrayBuffer = await storage.getFilePreview(
+          IMAGES_BUCKER_ID,
+          file.$id
+        );
+
+        uploadedImgUrl = `data:image/png;base64,${Buffer.from(
+          arrayBuffer
+        ).toString("base64")}`;
+      } else {
+        uploadedImgUrl = image;
+      }
+
+      const workspace = await databases.updateDocument(
+        DATABASE_ID,
+        WORKSPACE_ID,
+        workspaceId,
+        {
+          name,
+          imageUrl: uploadedImgUrl,
+        }
+      );
 
       return c.json({ data: workspace });
     }
--- src/features/workspaces/schemas.ts
+++ src/features/workspaces/schemas.ts
@@ -9,3 +9,13 @@ export const createWorkspaceSchema = z.object({
     ])
     .optional(),
 });
+
+export const updateWorkspaceSchema = z.object({
+  name: z.string().trim().min(1, "Atleast 1 character").optional(),
+  image: z
+    .union([
+      z.instanceof(File),
+      z.string().transform((value) => (value === "" ? undefined : value)),
+    ])
+    .optional(),
+});
